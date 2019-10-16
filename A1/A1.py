95728a5ac5b2f9785069da02cb611f92cf534075
Cameron Raymond <15cjkr@queensu.ca>
2019-10-16 15:05:52 -0400

    updated to prisoners dilemma

--- A1/A1.py
+++ A1/A1.py
@@ -1,62 +1,69 @@
 import numpy as np
-class PolicyIteration(object):
-    def __init__(self,p1_reward,p2_reward,alpha,k):
-        super().__init__()
-        self.p1_reward  = p1_reward
-        self.p2_reward  = p2_reward
-        self.actions    = [i for i in range(p1_reward.shape[0])]
-        self.p1_policy  = np.random.dirichlet(np.ones(p1_reward.shape[0])) # Initialize random simplex for taking each action
-        self.p2_policy  = np.random.dirichlet(np.ones(p1_reward.shape[0])) # Initialize random simplex for taking each action
-        self.alpha      = alpha
-        self.k          =  k
-        self.train()
-    
-    def normalize_values(self,matrix):
-        normalized = (matrix - np.min(matrix))/np.ptp(matrix)
-        return normalized
 
-    def softmax(self,x):
+class PolicyIteration(object):
+    def __init__(self, p1_reward, p2_reward, alpha, k,action_dict=None):
+        super().__init__()
+        self.p1_reward = p1_reward
+        self.p2_reward = p2_reward
+        self.actions = [i for i in range(p1_reward.shape[0])]
+        # Initialize random simplex for taking each action
+        self.p1_policy = np.random.dirichlet(np.ones(p1_reward.shape[0]))
+        # Initialize random simplex for taking each action
+        self.p2_policy = np.random.dirichlet(np.ones(p1_reward.shape[0]))
+        self.alpha = alpha
+        self.k = k
+        self.train(action_dict)
+
+    def softmax(self, x):
         """"
             Takes an n dimensional vector of real numbers, and normalizes it into a probability distribution 
             consisting of n probabilities proportional to the exponentials of the input numbers.
             https://en.wikipedia.org/wiki/Softmax_function
         """
         e_x = np.exp(x - np.max(x))
-        return e_x / e_x.sum(axis=0) # only difference
+        return e_x / e_x.sum(axis=0)  # only difference
 
-    def train(self):
-        action_exp ={0: "showed heads",1:"showed tails"}
+    def train(self,action_dict):
         for iteration in range(self.k):
-            p1_action       = np.random.choice(self.actions,p=self.p1_policy)
-            p2_action       = np.random.choice(self.actions,p=self.p2_policy)
-            self.p1_policy = self.update_policy(self.p1_policy,self.p1_reward,p1_action,p2_action)
-            self.p2_policy = self.update_policy(self.p2_policy,self.p2_reward,p1_action,p2_action)
-            if self.k < 10 or iteration%(self.k//10) == 0:
+            p1_action = np.random.choice(self.actions, p=self.p1_policy)
+            p2_action = np.random.choice(self.actions, p=self.p2_policy)
+            self.p1_policy = self.update_policy(self.p1_policy, self.p1_reward, p1_action, p2_action)
+            self.p2_policy = self.update_policy(self.p2_policy, self.p2_reward, p1_action, p2_action)
+            if self.k < 10 or iteration % (self.k//10) == 0:
                 print("Iteration: {}".format(iteration))
-                print("Player one {} and player two {}. Player one reward: {}. Player two reward: {}".format(action_exp[p1_action],action_exp[p2_action],self.p1_reward[p1_action,p2_action],self.p2_reward[p2_action,p1_action]))
-                print("Probability of user one showing heads: {}, Probability of showing tails: {}".format(self.p1_policy[0],self.p1_policy[1]))
-                print("Probability of user two showing heads: {}, Probability of showing tails: {}\n".format(self.p2_policy[0],self.p2_policy[1]))
+                print("Player one {} and player two {}. Player one reward: {}. Player two reward: {}".format(
+                    action_dict[p1_action], action_dict[p2_action], self.p1_reward[p1_action, p2_action], self.p2_reward[p2_action, p1_action]))
+                print("Player one policy {}".format(self.p1_policy))
+                print("Player two policy {}".format(self.p2_policy))
 
-    def update_policy(self,policy,reward,user_act,opponent_act):
-        act_reward = reward[user_act,opponent_act]
+
+    def update_policy(self, policy, reward, user_act, opponent_act):
+        # reward for an action also depends on what the other user did
+        act_reward = reward[user_act, opponent_act]
         act_probability = policy[user_act]
-        policy_delta    = self.alpha*act_reward*(1-act_probability)
+        # from equation 𝑝(𝑘 + 1) = 𝑝(𝑘) + 𝛼𝑟(𝑘)(1 − 𝑝(𝑘))
+        policy_delta = self.alpha*act_reward*(1-act_probability)
         policy[user_act] += policy_delta
+        # update all other probabilities 𝑝(𝑘 + 1) = 𝑝(𝑘) − 𝛼𝑟(𝑘)*𝑝(𝑘), 𝑓𝑜𝑟 𝑎𝑙𝑙 𝑜𝑡h𝑒𝑟 𝑎𝑐𝑡𝑖𝑜𝑛𝑠 𝑜 ≠ 𝑐
         for other_action in self.actions:
-                if other_action != user_act:
-                    act_probability = policy[other_action]
-                    policy_delta = self.alpha*act_reward*(act_probability)
-                    policy[other_action] -= policy_delta
-        policy = self.softmax(policy) #normalize probabilities
+            if other_action != user_act:
+                act_probability = policy[other_action]
+                policy_delta = self.alpha*act_reward*(act_probability)
+                policy[other_action] -= policy_delta
+        policy = self.softmax(policy)  # normalize probabilities
         return policy
 
 
 
-
 if __name__ == '__main__':
-    p1_head_tails = np.array([[1,-1],[-1,1]])
-    p1_head_tails = p1_head_tails
+    p1_head_tails = np.array([[1, -1], [-1, 1]])
     p2_head_tails = -p1_head_tails
-   
-    test = PolicyIteration(p1_head_tails,p2_head_tails,alpha=0.5,k=10000)
 
+    # heads_tails = PolicyIteration(p1_head_tails, p2_head_tails, alpha=0.1, k=80000,action_dict={0:"showed heads",1:"showed tails"})
+
+    p1_rps = np.array([[0,-1,1],[1,0,-1],[-1,1,0]])
+    p2_rps = -p1_rps
+    # rps = PolicyIteration(p1_rps, p2_rps, alpha=0.01, k=50000,action_dict={0:"threw rock",1:"threw paper",2:"threw scissors"})
+    p1_prisoners = np.array([[5, 10], [0, 1]])
+    p2_prisoners = np.transpose(p1_prisoners)
+    prisoners = PolicyIteration(p1_prisoners,p2_prisoners,alpha=0.01,k=50000,action_dict={0:"cooperates/lies",1:"confesses"})
