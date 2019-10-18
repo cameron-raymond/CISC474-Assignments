5c6391724140645c4a2f908a892f0e4e041265d8
Hugh <hugh.13@hotmail.com>
2019-10-18 13:39:52 -0400

    Initial commit of second algorithm

--- A1/A1.py
+++ A1/A1.py
@@ -1,9 +1,11 @@
 import numpy as np
+from numpy.core.multiarray import ndarray
+
 from visualizations import visualize_probabilities
 
 
 class PolicyIteration(object):
-    def __init__(self, p1_reward, p2_reward, alpha, k, action_dict=None,intermittent_normalization=True):
+    def __init__(self, p1_reward, p2_reward, alpha, k, action_dict=None,intermittent_normalization=True, second_algo_enable=False):
         super().__init__()
         self.p1_reward = p1_reward
         self.p2_reward = p2_reward
@@ -13,9 +15,12 @@ class PolicyIteration(object):
         self.p1_policy = np.random.dirichlet(np.ones(p1_reward.shape[0]))
         # Initialize random simplex for taking each action
         self.p2_policy = np.random.dirichlet(np.ones(p1_reward.shape[0]))
+        self.p1_policy_expectation = self.p1_policy
+        self.p2_policy_expectation = self.p2_policy
         self.alpha = alpha
         self.k = k
         self.action_dict = action_dict
+        self.second_algo_enable = second_algo_enable
 
     def softmax(self, x):
         """"
@@ -44,13 +49,16 @@ class PolicyIteration(object):
                 print("Player one reward: {}. Player two reward: {}".format(self.p1_reward[p1_action, p2_action], self.p2_reward[p1_action,p2_action]))
                 print("Player one policy {}".format(self.p1_policy))
                 print("Player two policy {}".format(self.p2_policy))
-            self.p1_policy = self.update_policy(self.p1_policy, self.p1_reward, p1_action, p2_action)
-            self.p2_policy = self.update_policy(self.p2_policy, self.p2_reward, p1_action, p2_action)
+            self.p1_policy, self.p1_policy_expectation = self.update_policy(self.p1_policy, self.p1_policy_expectation, self.p1_reward, p1_action, p2_action)
+            self.p2_policy, self.p2_policy_expectation = self.update_policy(self.p2_policy, self.p2_policy_expectation, self.p2_reward, p1_action, p2_action)
             p1_over_time = np.vstack([p1_over_time,self.p1_policy])
             p2_over_time = np.vstack([p2_over_time,self.p2_policy])
+        # Calculate value of game: P1*R1*P2^T
+        game_value = self.p1_policy*self.p1_reward*self.p2_policy.T
+        print("Game value: {}".format(game_value))
         return p1_over_time, p2_over_time
 
-    def update_policy(self, policy, reward, user_act, opponent_act):
+    def update_policy(self, policy, policy_expectation, reward, user_act, opponent_act):
         # reward for an action also depends on what the other user did
         act_reward = reward[user_act, opponent_act]
         act_probability = policy[user_act]
@@ -61,10 +69,16 @@ class PolicyIteration(object):
         policy_delta = -1.*self.alpha*act_reward*policy
         # bring in the delta from the action we actually chose
         policy_delta[user_act] = action_delta
+        if self.second_algo_enable:
+            # Calculate the expectation of the policy probabilities
+            policy_expectation_delta = self.alpha*(policy - policy_expectation)
+            policy_expectation += policy_expectation_delta
+            # Using the expectation, calculate the second term of α*E[p(k)] − p(k), and add it to policy_delta
+            policy_delta += self.alpha*(policy_expectation - policy)
         policy += policy_delta
         if not (policy > 0).all():
             policy = self.softmax(policy)  # normalize probabilities
-        return policy
+        return policy, policy_expectation
 
 if __name__ == '__main__':
     k=50000
@@ -81,7 +95,7 @@ if __name__ == '__main__':
                         [1, 0, -1], 
                         [-1, 1, 0]])
     p2_rps = -p1_rps
-    rps = PolicyIteration(p1_rps, p2_rps, alpha=0.001, k=k,action_dict={0:"threw rock",1:"threw paper",2:"threw scissors"})
+    rps = PolicyIteration(p1_rps, p2_rps, alpha=0.001, k=k,action_dict={0:"threw rock",1:"threw paper",2:"threw scissors"}, second_algo_enable=False)
     # p1_probs, p2_probs = rps.train()
     # visualize_probabilities(p1_probs,p2_probs,k+2,"Rock Paper Scissors Probability Chart",p1_labels=["P1 Rock","P1 Paper","P1 Scissors"],p2_labels=["P2 Rock","P2 Paper","P2 Scissors"])
 
@@ -89,7 +103,7 @@ if __name__ == '__main__':
     p1_pris = np.array([[ 5, 0], 
                         [10, 1]])
     p2_pris = p1_pris.T
-    pris = PolicyIteration(p1_pris, p2_pris, alpha=0.001, k=k,action_dict={0:"coop/lie to police",1:"defect/confess to police"},intermittent_normalization=False)
+    pris = PolicyIteration(p1_pris, p2_pris, alpha=0.001, k=k,action_dict={0:"coop/lie to police",1:"defect/confess to police"},intermittent_normalization=False, second_algo_enable=True)
     p1_probs, p2_probs = pris.train()
     visualize_probabilities(p1_probs,p2_probs,k+2,"Prisoners Dilemma Probability Chart",p1_labels=["P1 Cooperate","P1 Defect"],p2_labels=["P2 Cooperate","P2 Defect"])
 
