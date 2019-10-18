Fixed calculation of value of game

A1/A1.py
@@ -54,7 +54,7 @@ class PolicyIteration(object):
             p1_over_time = np.vstack([p1_over_time,self.p1_policy])
             p2_over_time = np.vstack([p2_over_time,self.p2_policy])
         # Calculate value of game: P1*R1*P2^T
-        game_value = self.p1_policy*self.p1_reward*self.p2_policy.T
+        game_value = self.p1_policy.dot(self.p1_reward.dot(self.p2_policy.T))
         print("Game value: {}".format(game_value))
         return p1_over_time, p2_over_time
 
@@ -86,9 +86,9 @@ if __name__ == '__main__':
     p1_head_tails = np.array([  [1, -1], 
                                 [-1, 1]])
     p2_head_tails = p1_head_tails*-1.
-    heads_tails = PolicyIteration(p1_head_tails, p2_head_tails, alpha=0.001, k=k,action_dict={0:"showed heads",1:"showed tails"})
-    # p1_probs, p2_probs = heads_tails.train()
-    # visualize_probabilities(p1_probs,p2_probs,k+2,"Dual Probability of Choosing Heads",p1_labels=["P1 Heads","P1 Tails"],p2_labels=["P2 Heads","P2 Tails"])
+    heads_tails = PolicyIteration(p1_head_tails, p2_head_tails, alpha=0.001, k=k,action_dict={0:"showed heads",1:"showed tails"}, second_algo_enable=True)
+    p1_probs, p2_probs = heads_tails.train()
+    visualize_probabilities(p1_probs,p2_probs,k+2,"Dual Probability of Choosing Heads",p1_labels=["P1 Heads","P1 Tails"],p2_labels=["P2 Heads","P2 Tails"])
    
     # ROCK PAPER SCISSORS 
     p1_rps = np.array([ [0, -1, 1], 
@@ -104,6 +104,6 @@ if __name__ == '__main__':
                         [10, 1]])
     p2_pris = p1_pris.T
     pris = PolicyIteration(p1_pris, p2_pris, alpha=0.001, k=k,action_dict={0:"coop/lie to police",1:"defect/confess to police"},intermittent_normalization=False, second_algo_enable=True)
-    p1_probs, p2_probs = pris.train()
-    visualize_probabilities(p1_probs,p2_probs,k+2,"Prisoners Dilemma Probability Chart",p1_labels=["P1 Cooperate","P1 Defect"],p2_labels=["P2 Cooperate","P2 Defect"])
+    # p1_probs, p2_probs = pris.train()
+    # visualize_probabilities(p1_probs,p2_probs,k+2,"Prisoners Dilemma Probability Chart",p1_labels=["P1 Cooperate","P1 Defect"],p2_labels=["P2 Cooperate","P2 Defect"])
 