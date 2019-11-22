4dc4db63f14705b38750f7996ed6b55f2c047dab
Zane Little <zanetlittle@gmail.com>
2019-11-22 13:08:39 -0500

    re-enable all algs in main

--- A2/A2.py
+++ A2/A2.py
@@ -13,69 +13,69 @@ epsilon = 0.01
 
 
 if __name__ == "__main__":
-    # # Performing Q-learning with standard environment
-    # q_agent = Q_Learning(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=False)
-    # print("--- starting training for Q Learning agent --- ")
-    # episode_steps = q_agent.train()
-    # print("--- optimal policy for Q Learning agent ---")
-    # q_agent.get_optimal_policy()
-    # print("--- plotting training for Q Learning agent ---")
-    # q_agent.plot(episode_steps)
+    # Performing Q-learning with standard environment
+    q_agent = Q_Learning(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=False)
+    print("--- starting training for Q Learning agent --- ")
+    episode_steps = q_agent.train()
+    print("--- optimal policy for Q Learning agent ---")
+    q_agent.get_optimal_policy()
+    print("--- plotting training for Q Learning agent ---")
+    q_agent.plot(episode_steps)
 
-    # # Performing Q-learning with standard environment, using eligibility trace
-    # q_agent = Q_Learning(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=False, _lambda=0.9)
-    # print("--- starting training for Q Learning agent using eligibility trace --- ")
-    # episode_steps = q_agent.train_lambda()
-    # print("--- optimal policy for Q Learning agent using eligibility trace ---")
-    # q_agent.get_optimal_policy()
-    # print("--- plotting training for Q Learning agent using eligibility trace ---")
-    # q_agent.plot(episode_steps)
+    # Performing Q-learning with standard environment, using eligibility trace
+    q_agent = Q_Learning(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=False, _lambda=0.9)
+    print("--- starting training for Q Learning agent using eligibility trace --- ")
+    episode_steps = q_agent.train_lambda()
+    print("--- optimal policy for Q Learning agent using eligibility trace ---")
+    q_agent.get_optimal_policy()
+    print("--- plotting training for Q Learning agent using eligibility trace ---")
+    q_agent.plot(episode_steps)
 
-    # Performing SARSA with standard environment
-    # sarsa_agent = SARSA(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=False)
-    # print("--- starting training for SARSA agent --- ")
-    # episode_steps = sarsa_agent.train()
-    # print("--- optimal policy for SARSA agent ---")
-    # sarsa_agent.get_optimal_policy()
-    # print("--- plotting training for SARSA agent ---")
-    # sarsa_agent.plot(episode_steps)
+    Performing SARSA with standard environment
+    sarsa_agent = SARSA(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=False)
+    print("--- starting training for SARSA agent --- ")
+    episode_steps = sarsa_agent.train()
+    print("--- optimal policy for SARSA agent ---")
+    sarsa_agent.get_optimal_policy()
+    print("--- plotting training for SARSA agent ---")
+    sarsa_agent.plot(episode_steps)
 
-    # # Performing SARSA with standard environment using eligibility trace
-    # sarsa_agent = SARSA(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=False, _lambda=0.9)
-    # print("--- starting training for SARSA agent using eligibility trace --- ")
-    # episode_steps = sarsa_agent.train()
-    # print("--- optimal policy for SARSA agent using eligibility trace ---")
-    # sarsa_agent.get_optimal_policy()
-    # print("--- plotting training for SARSA agent using eligibility trace ---")
-    # sarsa_agent.plot(episode_steps)
+    # Performing SARSA with standard environment using eligibility trace
+    sarsa_agent = SARSA(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=False, _lambda=0.9)
+    print("--- starting training for SARSA agent using eligibility trace --- ")
+    episode_steps = sarsa_agent.train()
+    print("--- optimal policy for SARSA agent using eligibility trace ---")
+    sarsa_agent.get_optimal_policy()
+    print("--- plotting training for SARSA agent using eligibility trace ---")
+    sarsa_agent.plot(episode_steps)
 
-    # # Performing Q-learning with king's moves and stochastic wind
-    # q_agent_king = Q_Learning(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=True)
-    # print("--- starting training for Q Learning agent with king's moves --- ")
-    # episode_steps = q_agent_king.train()
-    # print("--- optimal policy for Q Learning agent with king's moves ---")
-    # q_agent_king.get_optimal_policy()
-    # print("--- plotting training for Q Learning agent ---")
-    # q_agent_king.plot(episode_steps)
+    # Performing Q-learning with king's moves and stochastic wind
+    q_agent_king = Q_Learning(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=True)
+    print("--- starting training for Q Learning agent with king's moves --- ")
+    episode_steps = q_agent_king.train()
+    print("--- optimal policy for Q Learning agent with king's moves ---")
+    q_agent_king.get_optimal_policy()
+    print("--- plotting training for Q Learning agent ---")
+    q_agent_king.plot(episode_steps)
 
-    # # Performing Q-learning with king's moves and stochastic wind, using eligibility trace
-    # q_agent = Q_Learning(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=True, _lambda=0.9)
-    # print("--- starting training for Q Learning agent with king's moves and eligibility trace --- ")
-    # episode_steps = q_agent.train_lambda()
-    # print("--- optimal policy for Q Learning agent with king's moves and eligibility trace ---")
-    # q_agent.get_optimal_policy()
-    # print("--- plotting training for Q Learning agent with king's moves and eligibility trace ---")
-    # q_agent.plot(episode_steps)
+    # Performing Q-learning with king's moves and stochastic wind, using eligibility trace
+    q_agent = Q_Learning(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=True, _lambda=0.9)
+    print("--- starting training for Q Learning agent with king's moves and eligibility trace --- ")
+    episode_steps = q_agent.train_lambda()
+    print("--- optimal policy for Q Learning agent with king's moves and eligibility trace ---")
+    q_agent.get_optimal_policy()
+    print("--- plotting training for Q Learning agent with king's moves and eligibility trace ---")
+    q_agent.plot(episode_steps)
 
 
-    # # Performing SARSA with king's moves and stochastic wind
-    # sarsa_agent_king = SARSA(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=True)
-    # print("--- starting training for SARSA agent with king's moves --- ")
-    # episode_steps = sarsa_agent_king.train()
-    # print("--- optimal policy for SARSA agent with king's moves ---")
-    # sarsa_agent_king.get_optimal_policy()
-    # print("--- plotting training for SARSA agent with king's moves ---")
-    # sarsa_agent_king.plot(episode_steps)
+    # Performing SARSA with king's moves and stochastic wind
+    sarsa_agent_king = SARSA(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=True)
+    print("--- starting training for SARSA agent with king's moves --- ")
+    episode_steps = sarsa_agent_king.train()
+    print("--- optimal policy for SARSA agent with king's moves ---")
+    sarsa_agent_king.get_optimal_policy()
+    print("--- plotting training for SARSA agent with king's moves ---")
+    sarsa_agent_king.plot(episode_steps)
 
     # Performing SARSA with king's moves and stochastic wind using eligibility trace
     sarsa_agent = SARSA(episodes=episodes,lr=learning_rate,discount=discount,epsilon=epsilon,king=True, _lambda=0.9)
